84b3493b4cbcda5c728e95ff01ff1198c21aeb69
Gleidson Rodrigues Nunes <gleidson-nunes@hotmail.com>
2022-05-04 17:10:54 -0300

    Update
    Signed-off-by: Gleidson Rodrigues Nunes <gleidson-nunes@hotmail.com>

--- Main.cs
+++ Main.cs
@@ -21,7 +21,7 @@ namespace openencoder
         {
             Environment.SetEnvironmentVariable("ASPNETCORE_URLS", string.Empty);
             Console.Title = "Openencoder - Open Source Cloud Encoder";
-            CommandLine.Parser.Default.ParseArguments<Options>(args)
+            Parser.Default.ParseArguments<Options>(args)
                    .WithParsed(o =>
                    {
                        if (o.Server)
@@ -36,41 +36,40 @@ namespace openencoder
                            Console.WriteLine($"Running with {Environment.ProcessorCount} CPUs");
                            IHost host = Host.CreateDefaultBuilder(args).Build();
                            JobManager.Initialize();
+                           IConfigurationRoot config = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+                           OpenEncoderModel model = new();
+                           IModel channel = new ConnectionFactory { Uri = new Uri(config.GetValue<string>("RMQConnectionString")) }.CreateConnection().CreateModel();
                            JobManager.AddJob(
-                               () =>
+                           () =>
+                           {
+                               try
                                {
-                                   try
+
+                                   List<jobs> jobs = model.jobs.Where(a => (new string[] { "queued", "restarting" }).Contains(a.status)).ToList();
+                                   jobs.ForEach(a =>
                                    {
-                                       using OpenEncoderModel model = new();
-                                       List<jobs> jobs = model.jobs.Where(a => (new string[] { "queued", "restarting" }).Contains(a.status)).ToList();
-                                       using (IModel channel = new ConnectionFactory { Uri = new("amqp://gleidson:Gleidson@gleidsonnunes.loca.lt/") }.CreateConnection().CreateModel())
+                                       IBasicProperties props = channel.CreateBasicProperties();
+                                       props.Headers = new Dictionary<string, object>
                                        {
-                                           jobs.ForEach(a =>
-                                           {
-                                               IBasicProperties props = channel.CreateBasicProperties();
-                                               props.Headers = new Dictionary<string, object>
-                                               {
                                                    { "x-deduplication-header", true }
-                                               };
-                                               channel.BasicPublish(exchange: "", routingKey: "queue", basicProperties: props, body: Encoding.UTF8.GetBytes(Convert.ToBase64String(Encoding.UTF8.GetBytes($"{{\"guid\": \"{a.guid}\", \"preset\": \"{a.preset}\", \"source\": \"{a.source}\", \"destination\": \"{a.destination}\"}}"))));
-                                           });
-                                           EventingBasicConsumer? consumer = new(channel);
-                                           consumer.Received += (model, ea) =>
-                                           {
-                                               byte[]? body = ea.Body.ToArray();
-                                               string? message = Encoding.UTF8.GetString(body);
-                                               Console.WriteLine(message);
-                                           };
-                                           channel.BasicConsume(queue: "downstream", autoAck: true, consumer: consumer);
-                                       }
-                                   }
-                                   catch (Exception ex)
+                                       };
+                                       channel.BasicPublish(exchange: "", routingKey: "queue", basicProperties: props, body: Encoding.UTF8.GetBytes(Convert.ToBase64String(Encoding.UTF8.GetBytes($"{{\"guid\": \"{a.guid}\", \"preset\": \"{a.preset}\", \"source\": \"{a.source}\", \"destination\": \"{a.destination}\"}}"))));
+                                   });
+                                   EventingBasicConsumer? consumer = new(channel);
+                                   consumer.Received += (model, ea) =>
                                    {
-                                       Console.WriteLine(ex);
-                                   }
-                               },
-                               s => s.ToRunNow().AndEvery(5).Seconds()
-                           );
+                                       byte[]? body = ea.Body.ToArray();
+                                       string? message = Encoding.UTF8.GetString(body);
+                                       Console.WriteLine(message);
+                                   };
+                                   channel.BasicConsume(queue: "downstream", autoAck: true, consumer: consumer);
+                               }
+                               catch (Exception ex)
+                               {
+                                   Console.WriteLine(ex);
+                               }
+                           },
+                           s => s.ToRunNow().AndEvery(5).Seconds());
                            host.Run();
                        }
                    });
