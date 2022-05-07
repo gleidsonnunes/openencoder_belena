3f95d785e3b296fcf6b45cb742de4954c18ec21d
Gleidson Rodrigues Nunes <gleidson-nunes@hotmail.com>
2022-05-07 15:25:14 -0300

    Atualização e Bugfix
    Signed-off-by: Gleidson Rodrigues Nunes <gleidson-nunes@hotmail.com>

--- OpenEncoderGUI/Models/Context.Designer.cs
+++ OpenEncoderGUI/Models/Context.Designer.cs
@@ -11,12 +11,7 @@
 using System;
 using Devart.Data.Linq;
 using Devart.Data.Linq.Mapping;
-using System.Data;
 using System.ComponentModel;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Linq;
-using System.Linq.Expressions;
 
 namespace Model
 {
